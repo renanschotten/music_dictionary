4fa5c7d5b0f897c448d7eeccd7c06a7006ab3f76
Renan Schotten <renan.schotten@gmail.com>
2023-04-12 20:14:49 -0300

    feature: music dictionary local persistence

---
+++ lib/data/persistence/music_dictionary_persistence.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+import 'package:music_dictionary/data/models/app_content_model.dart';
+import 'package:music_dictionary/domain/entities/app_content.dart';
+import 'package:music_dictionary/shared/constants/shared_preferences_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class MusicDictionaryPersistence {
+  Future<bool> saveHomePageData(List<AppContent> homePageData);
+  Future<List<AppContentModel>?> fetchHomePage();
+}
+
+class SharedPrefsMusicDictionaryPersistence
+    implements MusicDictionaryPersistence {
+  late SharedPreferences sharedPreferences;
+
+  @override
+  Future<bool> saveHomePageData(List<AppContent> homePageData) async {
+    try {
+      sharedPreferences = await SharedPreferences.getInstance();
+      List<Map<String, dynamic>> map = homePageData
+          .map((e) => AppContentModel.fromEntity(e).toMap())
+          .toList();
+      String json = jsonEncode(map);
+      await sharedPreferences.setString(LocalStorageKeys.homePageData, json);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  @override
+  Future<List<AppContentModel>?> fetchHomePage() async {
+    try {
+      sharedPreferences = await SharedPreferences.getInstance();
+      final json = sharedPreferences.getString(
+        LocalStorageKeys.homePageData,
+      );
+      if (json == null || json.isEmpty) return null;
+
+      final List response = jsonDecode(json);
+      final List<AppContentModel> homePageData =
+          response.map((e) => AppContentModel.fromMap(e)).toList();
+      return homePageData;
+    } catch (e) {
+      return null;
+    }
+  }
+}
