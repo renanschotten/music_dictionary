b369f33927e5af631911f132e4bbdaece9fb4e83
Renan Schotten <renan.schotten@gmail.com>
2023-04-16 19:01:46 -0300

    chore: added chords route and firebase keys

--- lib/app/music_dictionary_app.dart
+++ lib/app/music_dictionary_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:music_dictionary/app/di/injection_container.dart';
 import 'package:music_dictionary/domain/services/music_dictionary_service.dart';
+import 'package:music_dictionary/presentation/pages/chords_page/chords_page.dart';
 import 'package:music_dictionary/presentation/pages/home_page/bloc/home_page_bloc.dart';
 import 'package:music_dictionary/presentation/pages/home_page/home_page.dart';
 import 'package:music_dictionary/shared/config/routes.dart';
@@ -26,6 +27,7 @@ class MusicDictionaryApp extends StatelessWidget {
         initialRoute: Routes.home,
         routes: {
           Routes.home: (_) => HomePage(),
+          Routes.chords: (_) => ChordsPage(),
         },
       ),
     );
--- lib/shared/constants/firebase_keys.dart
+++ lib/shared/constants/firebase_keys.dart
@@ -1,4 +1,6 @@
 class FirebaseKeys {
   static const String homePageCollection = 'home_page';
   static const String homePageContents = 'home_page_contents';
+  static const String chordsPageCollection = 'chords_page';
+  static const String chordsPageContents = 'chords_page_contents';
 }
