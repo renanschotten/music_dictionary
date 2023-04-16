b4535503190218feb5f2cd60d196b08d353fff49
Renan Schotten <renan.schotten@gmail.com>
2023-04-16 16:26:11 -0300

    feat: created method to clear sharedpreferences for testing

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:music_dictionary/app/music_dictionary_app.dart';
 import 'package:music_dictionary/firebase_options.dart';
 import 'package:music_dictionary/app/di/injection_container.dart' as ic;
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await ic.init();
+  await clearSharedPrefs();
   runApp(const MusicDictionaryApp());
+}
+
+Future<void> clearSharedPrefs() async {
+  final sp = await SharedPreferences.getInstance();
+  sp.clear();
 }
