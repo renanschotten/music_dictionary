de74c877b898465abfdc7a5ee129535191497610
Renan Schotten <renan.schotten@gmail.com>
2023-04-17 19:56:39 -0300

    feat: persistence layer

--- lib/data/persistence/music_dictionary_persistence.dart
+++ lib/data/persistence/music_dictionary_persistence.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:music_dictionary/data/models/app_content_model.dart';
+import 'package:music_dictionary/data/models/chord_model.dart';
 import 'package:music_dictionary/domain/entities/app_content.dart';
 import 'package:music_dictionary/shared/constants/shared_preferences_keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,13 +9,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 abstract class MusicDictionaryPersistence {
   Future<bool> saveHomePageData(List<AppContent> homePageData);
   Future<List<AppContentModel>?> fetchHomePage();
+  Future<List<ChordModel>?> fetchChordsPage();
 }
 
 class SharedPrefsMusicDictionaryPersistence
     implements MusicDictionaryPersistence {
   late SharedPreferences sharedPreferences;
-
-  //SharedPrefsMusicDictionaryPersistence(this.sharedPreferences);
 
   @override
   Future<bool> saveHomePageData(List<AppContent> homePageData) async {
@@ -44,6 +44,21 @@ class SharedPrefsMusicDictionaryPersistence
       final List<AppContentModel> homePageData =
           response.map((e) => AppContentModel.fromMap(e)).toList();
       return homePageData;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  Future<List<ChordModel>?> fetchChordsPage() async {
+    try {
+      sharedPreferences = await SharedPreferences.getInstance();
+      final json = sharedPreferences.getString(LocalStorageKeys.chordsPageData);
+      if (json == null || json.isEmpty) return null;
+      final List response = jsonDecode(json);
+      final List<ChordModel> chordsPageData =
+          response.map((e) => ChordModel.fromMap(e)).toList();
+      return chordsPageData;
     } catch (e) {
       return null;
     }
