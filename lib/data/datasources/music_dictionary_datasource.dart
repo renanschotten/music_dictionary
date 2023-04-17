fba87ad64638f36017daaf075ae07c09b045f4e7
Renan Schotten <renan.schotten@gmail.com>
2023-04-17 19:52:52 -0300

    feat: datasource

--- lib/data/datasources/music_dictionary_datasource.dart
+++ lib/data/datasources/music_dictionary_datasource.dart
@@ -2,11 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 
 import 'package:music_dictionary/data/models/app_content_model.dart';
+import 'package:music_dictionary/data/models/chord_model.dart';
 import 'package:music_dictionary/shared/constants/firebase_keys.dart';
 import 'package:music_dictionary/shared/core/failure.dart';
 
 abstract class MusicDictionaryDatasource {
   Future<Either<Failure, List<AppContentModel>>> fetchHomePage();
+  Future<Either<Failure, List<ChordModel>>> fetchChordsPage();
 }
 
 class MockMusicDictionaryDatasource implements MusicDictionaryDatasource {
@@ -16,6 +18,15 @@ class MockMusicDictionaryDatasource implements MusicDictionaryDatasource {
     return Right([
       AppContentModel(name: 'Acordes', path: '/chords'),
       AppContentModel(name: 'Escalas', path: '/scales')
+    ]);
+  }
+
+  @override
+  Future<Either<Failure, List<ChordModel>>> fetchChordsPage() async {
+    await Future.delayed(Duration(seconds: 2));
+    return Right([
+      ChordModel(name: 'A', images: ['img1'], description: 'description'),
+      ChordModel(name: 'B', images: ['img1'], description: 'description'),
     ]);
   }
 }
@@ -34,6 +45,33 @@ class FirestoreMusicDictionaryDatasource implements MusicDictionaryDatasource {
       final List array = response.data()?['contents'];
       array.forEach((e) => homePageData.add(AppContentModel.fromMap(e)));
       return Right(homePageData);
+    } on FirebaseException catch (e) {
+      return Left(
+        NetworkFailure(
+          message: e.message ?? 'Erro de Conexao',
+          code: e.code,
+        ),
+      );
+    } catch (e) {
+      return Left(
+        GenericFailure(
+          message: e.toString(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<ChordModel>>> fetchChordsPage() async {
+    try {
+      List<ChordModel> chordsPageData = [];
+      final response = await firestore
+          .collection(FirebaseKeys.chordsPageCollection)
+          .doc(FirebaseKeys.chordsPageContents)
+          .get();
+      final List array = response.data()?['contents'];
+      array.forEach((e) => chordsPageData.add(ChordModel.fromMap(e)));
+      return Right(chordsPageData);
     } on FirebaseException catch (e) {
       return Left(
         NetworkFailure(
