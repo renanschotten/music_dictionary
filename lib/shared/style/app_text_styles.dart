a549b61ebeef362137b9886a1c42d38d92f24734
Renan Schotten <renan.schotten@gmail.com>
2023-04-21 19:14:20 -0300

    feature: added montserrat font and created first text style

---
+++ lib/shared/style/app_text_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle montserrat24w900 = TextStyle(
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w900,
+    fontSize: 24,
+  );
+}
